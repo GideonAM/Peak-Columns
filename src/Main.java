88b3661d8b739ed132acfa84f31c7e05daa19da1
Gideon Agbosu <gideon.agbosu@amalitech.com>
2024-11-07 15:21:52 +0000

    Add peakColums method

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,52 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[][] A = {{1, 8, 3, 4, 0},
+                     {6, 7, 2, 7, 0}};
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int[][] B = {{3, 5, 6, 7, 7},
+                    {4, 2, 2, 8, 9},
+                    {6, 3, 2, 9, 7}};
+
+        int[][] C = {{12, 2, 4},
+                {17, 10, 1},
+                {92, 80, 79}};
+
+        peakColumns(A);
+        peakColumns(B);
+        peakColumns(C);
+    }
+
+    private static void peakColumns(int[][] inputArray) {
+/*       get max element in subArray
+         get index of max element in subArray
+         if max element is the minimum in its column print it and its index
+          */
+
+        try {
+            for (int i = 0; i < inputArray.length; i++) {
+                int maxElementInRow = Arrays.stream(inputArray[i]).max().orElse(0);
+
+                for (int j = 0; j < inputArray[i].length; j++) {
+                    if (inputArray[i][j] >= maxElementInRow) {
+                        var temp = new ArrayList<Integer>();
+                        for (int[] element : inputArray) {
+                            temp.add(element[j]);
+                        }
+
+                        var minElementInColumn = temp.stream().min(Integer::compareTo).get();
+                        if (maxElementInRow <= minElementInColumn)
+                            System.out.printf("(%s,%s) = %s", i + 1, j + 1, maxElementInRow);
+
+                        System.out.println();
+                    }
+                }
+            }
+        } catch (Exception exception) {
+            System.out.println("Ensure given matrix is in the right form");
         }
     }
+
 }
