5b9541f8eaeb6408d400e50cf2a3b11c2be4cd2e
Gideon Agbosu <gideon.agbosu@amalitech.com>
2024-11-08 20:20:40 +0000

    Handled exceptions

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Enter number of rows for matrix");
-        Scanner userInput = new Scanner(System.in);
-        int rowOneInput = userInput.nextInt();
+        try {
+            System.out.println("Enter number of rows for matrix");
+            Scanner userInput = new Scanner(System.in);
+            int rowOneInput = userInput.nextInt();
 
-        System.out.println("Enter number of columns for matrix");
-        int columnOneInput = userInput.nextInt();
-        int[][] matrix = new int[rowOneInput][columnOneInput];
+            System.out.println("Enter number of columns for matrix");
+            int columnOneInput = userInput.nextInt();
+            int[][] matrix = new int[rowOneInput][columnOneInput];
 
-        // for inserting elements into matrix
-        System.out.println("Enter matrix elements one row at a time: ");
-        for (int i = 0; i < matrix.length; i++)
-            for (int j = 0; j < matrix[0].length; j++)
-                matrix[i][j] = userInput.nextInt();
+            // for inserting elements into matrix
+            System.out.println("Enter matrix elements one row at a time: ");
+            for (int i = 0; i < matrix.length; i++)
+                for (int j = 0; j < matrix[0].length; j++)
+                    matrix[i][j] = userInput.nextInt();
 
-        System.out.println("Input 2D array " + Arrays.deepToString(matrix));
-        peakColumns(matrix);
+            System.out.println("Input 2D array " + Arrays.deepToString(matrix));
+            peakColumns(matrix);
+        } catch (Exception exception) {
+            System.out.println("Enter numbers only");
+        }
     }
 
     private static void peakColumns(int[][] inputArray) {
